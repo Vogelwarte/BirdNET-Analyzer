0e08f4573dfcf4e1dbdd161d7242fa502c9191a8
Mario Fischer <mario.fischer@vogelwarte.ch>
2024-10-22 13:12:52 +0200

    results writing to file works

--- analyzePipelined.py
+++ analyzePipelined.py
@@ -5,18 +5,29 @@ import librosa as lr
 import config as cfg
 import os
 import utils
+import json
 from pathlib import Path
 from collections import defaultdict
+
+def loadCodes():
+    """Loads the eBird codes.
+
+    Returns:
+        A dictionary containing the eBird codes.
+    """
+    with open(os.path.join(SCRIPT_DIR, cfg.CODES_FILE), "r") as cfile:
+        codes = json.load(cfile)
+
+    return codes
 
 SCRIPT_DIR = os.path.abspath('')
 cfg.LABELS_FILE = os.path.join(SCRIPT_DIR, cfg.LABELS_FILE)
 cfg.LABELS = utils.readLines(cfg.LABELS_FILE)
-cfg.MIN_CONFIDENCE = 0.5
+cfg.MIN_CONFIDENCE = 0.1
 cfg.SIGMOID_SENSITIVITY = max(0.5, min(1.0 - (1 - 1.0), 1.5))
 cfg.LABELS_FILE = os.path.join(SCRIPT_DIR, cfg.LABELS_FILE)
 cfg.LABELS = np.array(utils.readLines(cfg.LABELS_FILE))
-
-
+cfg.CODES = loadCodes()
 
 output_folder = "./example/vowa/output"
 def result_writer(filename, segment_start, segment_end, predictions):
@@ -30,19 +41,6 @@ def result_writer(filename, segment_start, segment_end, predictions):
         with open(output_file, "a") as file:
             file.write(f"{segment_start}\t{segment_end}\t{species}\t{confidence}\n")
     [map_fn(segment_start, segment_end,*prediction) for prediction in predictions]
-
-#def gather_result(filenames: np.ndarray, windows: np.ndarray, batch_predictions:np.ndarray):
-    #relevant_predictions = batch_predictions > cfg.MIN_CONFIDENCE
-
-    
-
-    # for filename, window, predicitons in zip(filenames, windows, batch_predictions):
-    #     s_start = window * 3
-    #     s_end = s_start + 3
-    #     p_labels = filter(lambda x: x[1] > cfg.MIN_CONFIDENCE, zip(cfg.LABELS, predictions))
-    #     p_sorted = sorted(p_labels, key=operator.itemgetter(1), reverse=True)
-    #     gather.append((filename, s_start, s_end, p_sorted))
-    # return gather
 
 classifier = tf.keras.models.load_model(os.path.join(SCRIPT_DIR, cfg.MODEL_PATH), compile=True)
 
@@ -68,24 +66,10 @@ def predict(chunk, chunk_info):
     sigmoid = 1 / (1 + tf.exp(-1 * tf.clip_by_value(logits, -15, 15)))
     return (filename, window, sigmoid)
 
-# @tf.function
-# def (filename, window, sigmoid):
-    
-
 
 ds = tf.data.Dataset.list_files("example/vowa/*.flac")
 ds = ds.map(decode_flac,num_parallel_calls=tf.data.AUTOTUNE)
 ds = ds.interleave(create_slices, num_parallel_calls=tf.data.AUTOTUNE)
-
-# res = []
-# for batch in ds.batch(50).prefetch(tf.data.AUTOTUNE):
-#     res.append(predict(*batch))
-
-# # for batch in ds.batch(50).map(predict, num_parallel_calls=tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE):
-# #     res.append(batch)
-
-
-# print(res)
 
 it = ds.batch(50).prefetch(tf.data.AUTOTUNE)
 filenames = []
@@ -99,29 +83,43 @@ filenames = np.concatenate(filenames)
 windows = np.concatenate(windows)
 predictions = np.concatenate(predictions)
 
-print(filenames.shape)
-print(windows.shape)
-print(predictions.shape)
-# for b_filename, b_windows, b_sigmoids in ds.batch(50).map(predict).prefetch(tf.data.AUTOTUNE):
-#     filenames.append(b_filename), windows.append(b_windows), sigmoids.append(b_sigmoids)
+# Write the results file
+# 1. Get all unique filenames of the processed data
+unique_filenames = np.unique(filenames)
 
+# create the filtered set of windows and predictions
+pred_above_min_conf_flag = predictions > cfg.MIN_CONFIDENCE
+arg_filtered_predictions = np.where(pred_above_min_conf_flag.any(axis=1))[0]
+filtered_predictions = predictions[arg_filtered_predictions]
+filtered_windows = windows[arg_filtered_predictions]
+filtered_filenames = filenames[arg_filtered_predictions]
+filtered_pred_above_min_conf_flag = pred_above_min_conf_flag[arg_filtered_predictions]
 
-# result_dict = defaultdict(list)
- 
-# # List comprehension to perform the same operation
-# x = [ result_dict[filename.numpy()].append((window, predictions)) for filename, window, predictions in it ]
+# 2. loop over the files
+for filename in unique_filenames:
+    with open(Path(output_folder) / Path(f"{Path(str(filename)).stem}.fast.txt"), "a") as output_file:
+        # Write header
+        output_file.write("Selection\tViewtChannel\tBegin Time (s)\tEnd Time (s)\tLow Freq (Hz)\tHigh Freq (Hz)\tCommon Name\tSpecies Code\tConfidence\tBegin Path\tFile Offset (s)")
 
-# for filename, window, predictions in it:
-#     result_dict[filename].append((window, predictions))
+        # select the relevant data for the current file
+        arg_current_filename = np.where(filtered_filenames == filename)
+        current_file_windows = filtered_windows[arg_current_filename]
+        current_file_predictions = filtered_predictions[arg_current_filename]
+        current_file_pred_above_min_conf_flag = filtered_pred_above_min_conf_flag[arg_current_filename]
 
+        # sort by window index
+        arg_sorted_current_windows = np.argsort(current_file_windows)
+        
+        for arg_window in arg_sorted_current_windows:
+            begin_time = current_file_windows[arg_window] * 3
+            end_time = begin_time + 3
+            
+            arg_current_file_predictions = np.where(current_file_pred_above_min_conf_flag[arg_window])
 
-# #[ result_dict[filename].append((window,predictions)) if (filename in result_dict) else result_dict = {**result_dict, filename: [(window, predictions)]} for filename, window, predictions in it ]
-# result_dict = {}
-# def gather_result(filename, window, predictions, result_dict):
-#     if filename in result_dict:
-#         result_dict[filename].append((window, predictions))
-#     else:
-#         result_dict[filename] = [(window, predictions)]
-
-# list(map(lambda x: gather_result(*x, result_dict), it))
-
+            predicitons_in_window = zip(current_file_predictions[arg_window][arg_current_file_predictions],cfg.LABELS[arg_current_file_predictions])
+            for confidence, label in predicitons_in_window:
+                code = cfg.CODES[label] if label in cfg.CODES else label
+                output_file.write(
+                    f"1\tSpectrogram 1\t1\t{begin_time}\t{end_time}\t0\t24000\t{label.split('_', 1)[-1]}\t{code}\t{confidence:.4f}\t{filename}\t{begin_time}\n"
+                )
+                
